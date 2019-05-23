59a3ac7a2969cb804d4150cca8d81bc1a63fc578
unknown <Digital@DH-A8-09.dhbrasil.digitalhouse.com>
2019-05-22 21:14:34 -0300

    fragmento

---
+++ FragmentApp/app/src/main/java/br/com/digiralhouse/fragmentapp/MainActivity.java
@@ -0,0 +1,55 @@
+package br.com.digiralhouse.fragmentapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import br.com.digiralhouse.fragmentapp.fragments.Fragment2;
+import br.com.digiralhouse.fragmentapp.fragments.fragmento1;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnFragment1;
+    private Button btnFragment2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+
+        btnFragment1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                addFragment(new fragmento1());
+            }
+        });
+
+        btnFragment2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addFragment(new Fragment2());
+            }
+        });
+    }
+
+    private void addFragment(Fragment fragment){
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.container, fragment);
+        transaction.addToBackStack("FRAGMENTS");
+        transaction.commit();
+    }
+
+    private void initViews() {
+        btnFragment1 = findViewById(R.id.btnFragmento1);
+        btnFragment2 = findViewById(R.id.btnFragmento2);
+    }
+}
---
+++ FragmentApp/app/src/main/java/br/com/digiralhouse/fragmentapp/fragments/fragmento1.java
@@ -0,0 +1,25 @@
+package br.com.digiralhouse.fragmentapp.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.digiralhouse.fragmentapp.R;
+
+public class fragmento1 extends Fragment {
+
+    public fragmento1(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_1, container, false);
+        return view;
+    }
+}
